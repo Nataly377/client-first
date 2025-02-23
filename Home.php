d73096db898dcd64af6e3a5597d18ca98f677520
Nataly377 <bogdanovicnatasa2@gmail.com>
2025-02-23 22:01:29 +0100

    client-first

---
+++ Home.php
@@ -0,0 +1,335 @@
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Client-first</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <!-- navbar -->
+
+    <div class="navbar">
+        <div class="container">
+            <div>
+                <img src="img/Logo.svg" alt="logo">
+                <a href="Home.php">Home</a>
+                <a onclick=idiNaMenu()>Menu</a>
+                <a href="About Us.php">About Us</a>
+                <a href="OurStory.php">OurStory</a>
+                <a href="Blog.php">Blog</a>
+                <a href="Contact.php">Contact</a>
+            </div>
+            <div class="arow">
+                <p class="me-2">Clone Project</p>
+                <img src="img/Arrow 6.png" alt="arow" class="ar">
+            </div>
+        </div>
+    </div>
+
+    <!-- header -->
+
+    <header class="header container d-md-flex justify-content-between">
+        <div>
+            <h1 class="pb-md-3">We serve high quality foods of all kinds.</h1>
+            <p class="pb-3">Edit this text to make it your own. To edit, simply click directly on the text to start
+                adding your own words. You can move the text.</p>
+            <button class="btn">View Menu
+                <img src="img/Arrow.png" class="btnar">
+            </button>
+            | <h4 class="mt-3">Opening Times</h4>
+            <p>Sunday to Saturday  l  09:00 AM to 11:00 PM</p>
+            <h4>Location</h4>
+            <p>Master canteen, BBSR , Odisha 752054</p>
+            <h4>Call us</h4>
+            <p>+9776462441</p>
+        </div>
+        <div class="heimg">
+            <img src="img/Header Image.jpg" alt="breaktime">
+        </div>
+    </header>
+
+    <!-- aboutus -->
+
+<article class="aboutus container">
+  <div class="d-md-flex justify-content-between">
+    <div class="text-area">
+        <p class="line">KNOW MORE ABOUT US<img src="img/Line.png" class="ms-3"></p>
+        <h2>We source sustainable & line caught Foods</h2>
+        <p class="pt-3">Edit this text to make it your own. To edit, simply click directly on the text to start
+        adding your own words. You can move the text by dragging and dropping the text anywhere on the page.
+        </p>
+    <div class="d-flex">
+    <div>
+        <p class="text"><img src="img/Icon.png" alt="icon" class="me-1">10+ People</p>
+        <p>We are Small Team</p>
+        <p class="pt-3">Through True Rich Attended does no end it his mother since favourable.</p>
+    </div>
+    <div>
+        <p class="text1"><img src="img/Icon.png" alt="icon" class="me-1">2014</p>
+        <p>We are From</p>
+        <p class="pt-3">Through True Rich Attended does no end it his mother since favourable.</p>
+      </div>
+    <div>
+        <p class="text2"><img src="img/Icon.png" alt="icon" class="me-1">200k</p>
+        <p>We Served</p>
+        <p class="pb-2 pt-3">Through True Rich Attended does no end it his mother since favourable.</p>
+      </div>
+    </div>
+<div>
+    <button class="btn">View Menu
+      <img src="img/Arrow.png" class="btnar">
+    </button>
+ </div>
+</div>
+ <div class="image-section d-flex justify-content-between">
+    <div>
+      <img src="img/Know more Image 1.jpg" alt="relaxtime" class="slika1 me-5">
+    </div>
+  <div>
+      <img src="img/Know more Image 2.jpg" alt="relaxtime">
+    </div>
+   </div>
+ </div>
+</article>
+
+    <!--serving -->
+
+    <div class="serving container d-md-flex justify-content-between mb-5">
+        <div>
+            <p class="wws mb-md-5">WHAT WE ARE SERVING<img src="img/Line.png" class="ms-2"></p>
+            <h2>We all have to eat, so why not do it beautifully?</h2>
+        </div>
+        <div class="ms-5 mt-md-5">
+            <p>Through True Rich Attended does no end it his mother since favourable real had half every him case in
+                packages enquire we up ecstatic.. Through True Rich Attended does no end it his mother</p>
+        </div>
+    </div>
+    <div class="menu d-md-flex container">
+        <div>
+            <img src="img/Icon (1).png" alt="icon">
+            <h3>Quafe Cake</h3>
+            <p class="mb-5">Through True Rich Attended no end it his mother since favourable real had half every him.
+            </p>
+            <img src="img/Serving 4.png" alt="icon" class="mt-5">
+            <h3>Cocktails</h3>
+            <p>Through True Rich Attended no end it his mother since favourable real had half every him.</p>
+        </div>
+        <div>
+            <img src="img/Vector.png" alt="icon" class="mt-4">
+            <h3>Cofee</h3>
+            <p class="mb-5">Through True Rich Attended no end it his mother since favourable real had half every him.
+            </p>
+            <img src="img/Icon (3).png" alt="icon" class="mt-5">
+            <h3>Fast Food</h3>
+            <p>Through True Rich Attended no end it his mother since favourable real had half every him.</p>
+        </div>
+        <div>
+            <img src="img/Serving 4.png" alt="icon">
+            <h3>Fast Food</h3>
+            <p class="mb-5">Through True Rich Attended no end it his mother since favourable real had half every him.
+            </p>
+            <img src="img/Icon (4).png" alt="icon" class="mt-5">
+            <h3>Snacks</h3>
+            <p>Through True Rich Attended no end it his mother since favourable real had half every him.</p>
+        </div>
+    </div>
+
+    <!-- ourmenu  -->
+
+    <div class="ourmenu container d-md-flex justify-content-between">
+        <div>
+            <p class="meni mb-5">OUR MENU<img src="img/Line.png" class="ms-2"></p>
+            <h2>Discover our menu chart</h2>
+            <h5>Most Popular Picks</h5>
+        </div>
+        <div class="paragraf">
+            <p>Through True Rich Attended does no end it his mother since favourable real had half every him case in
+                packages enquire we up ecstatic.. Through True Rich Attended does no end it his mother</p>
+        </div>
+    </div>
+
+    <div class=" container mt-5 mb-5">
+        <div class="slika">
+
+            <div class="me-2">
+                <img src="img/Image.jpg" alt="coffee">
+                <div class="item d-md-flex justify-md-content-between">
+                    <p class="mt-md-2">Drop Cofee</p>
+                    <p class="mt-md-2">$08.85</p>
+                </div>
+            </div>
+            <div class="me-md-2">
+                <img src="img/Image (1).png" alt="cake">
+                <div class="item d-md-flex justify-content-between">
+                    <p class="mt-2">Choco Cup Cake</p>
+                    <p class="mt-2">$4.85</p>
+                </div>
+            </div>
+            <div>
+                <img src="img/Image (2).png" alt="cake">
+                <div class="item d-md-flex justify-content-between">
+                    <p class="mt-2">Chicken Burger</p>
+                    <p class="mt-2">$8.85</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="slika1">
+            <div class="me-2">
+                <img src="img/Image (3).png" alt="cake">
+                <div class="item d-md-flex justify-content-between">
+                    <p class="mt-2">French Fries</p>
+                    <p class="mt-2">$7.85</p>
+                </div>
+            </div>
+            <div>
+                <img src="img/Image (4).png" alt="cake">
+                <div class="item d-flex justify-content-between">
+                    <p class="mt-2">Sandwitch</p>
+                    <p class="mt-2">$3.97</p>
+                </div>
+            </div>
+            <div class="ms-md-2">
+                <img src="img/Image (5).png" alt="cake">
+                <div class="item d-flex justify-content-between">
+                    <p class="mt-2">Chinese Soup</p>
+                    <p class="mt-2">$8.85</p>
+                </div>
+            </div>
+        </div>
+        <div class="dugme text-center">
+            <button class="btn">View Menu
+                <img src="img/Arrow.png">
+            </button>
+        </div>
+    </div>
+
+    <!-- quote -->
+
+    <div class="quote d-md-flex">
+        <div class="quote-text container">
+            <p class="qn mt-0">"</p>
+            <p class="qnn mt-1">Edit this text to make it your own. To edit, simply click directly on the text to start
+                adding your own words. You can move the text by dragging and dropping the text</p>
+            <p class="ja mb-0">Joheny Andro</p>
+            <p class="jaadres">Bhubaneswar, Odisha<img src="img/Img (1).jpg" alt="slika"
+                    class="rounded-circle mb-2 me-4" style="float: right;"></p>
+            <img src="img/Line (1).png">
+        </div>
+        <div class="quotevideo">
+            <img src="img/Video.jpg" alt="play" style="width: 100%;">
+        </div>
+    </div>
+
+
+    <!-- blog  -->
+
+    <div class="blog container">
+        <h2>Read Our Lastest Blog</h2>
+        <div class="d-md-flex justify-content-between mt-5">
+            <div class="blog-card me-4">
+                <p class="date px-3 py-3">21 Jun 2021</p>
+                <h3 class="px-3">Extra Thick Homemad Pizza Sauce</h3>
+                <p class="px-4 py-1">It is a long established fact that a reader will be distracted by the readable
+                    content of a page.</p>
+                <img src="img/blog1.jpg" alt="cofee" style="width: 100%;">
+            </div>
+            <div class="blog-card me-4">
+                <p class="date px-3 py-3">21 Jun 2021</p>
+                <h3 class="px-3">The Best Way to Store Fresh Herbs</h3>
+                <p class="px-4 py-4">It is a long established fact that a reader will be distracted by the readable
+                    content of a page.</p>
+                <img src="img/blog2.jpg" alt="cofee" style="width: 100%;">
+            </div>
+            <div class="blog-card">
+                <p class="date px-3 py-3">21 Jun 2021</p>
+                <h3 class="px-3">5 ways to prepare porridge</h3>
+                <p class="px-4 py-4">It is a long established fact that a reader will be distracted by the readable
+                    content of a page.</p>
+                <img src="img/blog3.jpg" alt="cofee" style="width: 100%;">
+            </div>
+        </div>
+    </div>
+
+    <!-- contact  -->
+<div class="contact container d-md-flex">
+  <div class="contactdata py-4 px-5">
+    <h4 class="mt-5">Working Hour</h4>
+    <p>Sunday to Saturday
+    09:00 AM to 11:00 PM</p>
+
+    <h4 class="mt-5">Location</h4>
+    <p>Street - 127, New York,United States
+    546544</p>
+
+    <h4 class="mt-5">Contact us </h4>
+    <p>+123456789
+    hey@yourdomain.com</p>
+   </div>
+  <div>
+      <img src="img/Map.png" alt="citymap" style="width: 100%;">
+  </div>
+</div>
+
+    <!-- footer-->
+
+<footer class="footer">
+    <img src="img/Logo.svg" alt="logo">
+  <div class="footer-link mb-5 mt-5">
+    <a href="home.php">Home</a>
+    <a href="Menu.php">Menu</a>
+    <a href="About Us">About Us</a>
+    <a href="OurStory">OurStory</a>
+    <a href="Blog">Blog</a>
+    <a href="Contact">Contact</a>
+  </div>
+<hr>
+  <div class="container d-flex justify-content-between">
+    <div>
+        <p>© 2021 Finsweet | All rights reserved.</p>
+    </div>
+    <div>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+      class="bi bi-facebook me-3" viewBox="0 0 16 16">
+      <path
+      d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
+      </svg>
+
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+      class="bi bi-twitter me-3" viewBox="0 0 16 16">
+      <path
+      d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334q.002-.211-.006-.422A6.7 6.7 0 0 0 16 3.542a6.7 6.7 0 0 1-1.889.518 3.3 3.3 0 0 0 1.447-1.817 6.5 6.5 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.32 9.32 0 0 1-6.767-3.429 3.29 3.29 0 0 0 1.018 4.382A3.3 3.3 0 0 1 .64 6.575v.045a3.29 3.29 0 0 0 2.632 3.218 3.2 3.2 0 0 1-.865.115 3 3 0 0 1-.614-.057 3.28 3.28 0 0 0 3.067 2.277A6.6 6.6 0 0 1 .78 13.58a6 6 0 0 1-.78-.045A9.34 9.34 0 0 0 5.026 15" />
+      </svg>
+
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+      class="bi bi-instagram me-3" viewBox="0 0 16 16">
+      <path
+      d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
+      </svg>
+
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+      class="bi bi-linkedin" viewBox="0 0 16 16">
+      <path
+      d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
+      </svg>
+      </div>
+      <div>
+      <p class="slikaf ms-2">Contact Us <img src="img/Arrow 6.png"></p>
+    </div>
+  <div>
+</footer>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
